fcabf839f7500b2c3363e3b9cd586dfceb5c4aa0
IWantWaffleFries1999 <FakeEmail@EMail.com>
2024-03-26 16:44:26 -0500

    quick bug fix introduced by Arch Fix branch

--- index.py
+++ index.py
@@ -40,10 +40,10 @@ def index():
         if matches_length > 0:
             message = str(f"Matches for: {game_tag}#{tag_line}")
             print(message)
-            Match_Detail_Dict = cpmdetail.get_match_detail(matches,puuid)
+            match_detail_dict = cpmdetail.get_match_detail(matches,puuid)
 
             return render_template('matches.html', form=form, message=message, matches_length=matches_length,
-                                   Match_Detail_Dict=Match_Detail_Dict, matches=matches, puuid=puuid)
+                                   match_detail_dict=match_detail_dict, matches=matches, puuid=puuid)
         else:
             message = str(f"{game_tag}#{tag_line} found but no recent matches. Check for typos and try again")
             print(message)

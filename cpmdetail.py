4609e90b80efb8b25225c6a3592fad79da508daf
Joseph Irving <jirving0311@gmail.com>
2024-09-21 19:22:45 -0400

    Added comparison to pro players but currently hard code limited

--- cpmdetail.py
+++ cpmdetail.py
@@ -1,7 +1,12 @@
 import configparser
 import models.match_detail
 import service.riotapi
+import pandas as pd
+
 from errors.not_found_error import NotFoundError
+
+csv_file_path = 'static/data/champ_expert_mapper.csv'
+champ_expert_mapper = pd.read_csv(csv_file_path)
 
 JUNGLE_CS = "jungle_cs"
 
@@ -11,34 +16,51 @@ config = configparser.ConfigParser()
 config.read_file(open(r'api-key.txt'))
 api_key = config.get('API Key', 'api_key')
 
+def ensure_string(value):
+    if isinstance(value, int):
+        return str(value)
+    return value
+
 try:
     def get_matches(puuid):
         return service.riotapi.get_matches(puuid, api_key, 5)
 
 
-    def get_match_detail(matches, puuid):
+    def get_matches_detail(matches, puuid):
+        print(f"Getting match details for {matches}:{puuid}")
         detailed_matches_dict = {}
-        for match in matches:
-            match_id = match
-            match_detail_dict = service.riotapi.get_match_detail(match_id, api_key)
-            match_detail_dict['puuid'] = puuid
-            participants_dict = match_detail_dict.get("info").get("participants")
-            participant_dict = [participant for (index, participant) in enumerate(participants_dict) if
-                                participant.get("puuid") == puuid]
-            participant_detail_dict = {k: v for participant_dict in participant_dict for (k, v) in
-                                       participant_dict.items()}
-            match_detail_dict = models.match_detail.merge(models.match_detail.MatchDetails(match_detail_dict),
-                                                          models.match_detail.ParticipantMatchDetails(
-                                                              participant_detail_dict))
-            detailed_matches_dict[match_id] = match_detail_dict
+        for match_id in matches:
+            detailed_matches_dict[match_id] = get_single_match_detail(match_id, puuid)
 
         return detailed_matches_dict
 
 
+    def get_single_match_detail(match_id, puuid):
+        match_detail_dict = service.riotapi.get_match_detail(match_id, api_key)
+        match_detail_dict['puuid'] = puuid
+        participants_dict = match_detail_dict.get("info").get("participants")
+        participant_dict = [participant for (index, participant) in enumerate(participants_dict) if
+                            participant.get("puuid") == puuid]
+        participant_detail_dict = {k: v for participant_dict in participant_dict for (k, v) in
+                                   participant_dict.items()}
+        match_details = models.match_detail.MatchDetails(match_detail_dict)
+        participant_details = models.match_detail.ParticipantMatchDetails(participant_detail_dict)
+        return models.match_detail.merge(match_details, participant_details)
+
+
+    def get_puuid_from_data(game_tag, tag_line):
+        return service.riotapi.get_puuid(game_tag, tag_line, api_key)
+
+
     def get_matches_from_user_input(game_tag, tag_line):
-        puuid = service.riotapi.get_puuid(game_tag, tag_line, api_key)
+        puuid = get_puuid_from_data(game_tag, tag_line)
         matches = get_matches(puuid)
-        return get_match_detail(matches, puuid)
+        return get_matches_detail(matches, puuid)
+
+
+    def get_last_match(puuid):
+        return service.riotapi.get_last_match(puuid, api_key)
+
 
     def get_match_timeline(match_id):
         # get match timeline
@@ -48,47 +70,57 @@ try:
 
 
     def get_cs_per_frame(participant_id, match_id):
+        participant_id = ensure_string(participant_id)
         frames = get_match_timeline(match_id)
         minute_count = 0
-        participant_events_per_minute = {}
+        participant_frames_array = {}
         events_per_minute = {}
         for frame in frames:
+            print(f"Got frame: {frame}")
             # gets participant info for a given minute
             participant_frame = frame.get("participantFrames").get(participant_id)
+            print(f"Getting participant frame {participant_frame} for participant ID {participant_id}")
             minions_killed = participant_frame.get("minionsKilled")
             jungle_minions_killed = participant_frame.get("jungleMinionsKilled")
             exp = participant_frame.get("xp")
-            participant_frame_dict = {LANE_CS: minions_killed, ("%s" % JUNGLE_CS): jungle_minions_killed, "exp": exp}
-            participant_events_per_minute[minute_count] = participant_frame_dict
+            level = participant_frame.get("level")
+            gold = participant_frame.get("totalGold")
+            participant_frame_dict = {LANE_CS: minions_killed, JUNGLE_CS: jungle_minions_killed, "exp": exp,
+                                      "level": level, "gold": gold}
+            participant_frames_array[minute_count] = participant_frame_dict
 
             # get events for a given minute
             events_this_minute = frame.get("events")
             participant_events_dict = {}
             participant_events_this_minute = 0
             for event in events_this_minute:
-                print(str(f"Event: {event}"))
+                # print(str(f"Event: {event}"))
                 event_participant_id = event.get("participantId")
-                print(str(f"Is this eventPID [{event_participant_id}] = the PID [{int(participant_id)}]? {event_participant_id == int(participant_id)}"))
+                # print(str(f"Is this eventPID [{event_participant_id}] = the PID [{int(participant_id)}]? {event_participant_id == int(participant_id)}"))
                 if event_participant_id is not None and int(event_participant_id) == int(participant_id):
-                    print(str(f"PID {event_participant_id} is not None and matches the user PID {participant_id}"))
-                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"), "context": "self"}
+                    # print(str(f"PID {event_participant_id} is not None and matches the user PID {participant_id}"))
+                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"),
+                                                                               "context": "self"}
 
                 elif "assistingParticipantIds" in event and int(participant_id) in event.get("assistingParticipantIds"):
-                    print(str(f"assistingParticipantIds is present and matches the user PID {participant_id}"))
-                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"), "context": "assist"}
+                    # print(str(f"assistingParticipantIds is present and matches the user PID {participant_id}"))
+                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"),
+                                                                               "context": "assist"}
 
                 elif "killerId" in event and int(participant_id) == event.get("killerId"):
-                    print(str(f"killerId is present and matches the user PID {participant_id}"))
-                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"), "context": "killer"}
+                    # print(str(f"killerId is present and matches the user PID {participant_id}"))
+                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"),
+                                                                               "context": "killer"}
 
                 elif "victimId" in event and int(participant_id) == event.get("victimId"):
-                    print(str(f"victimId is present and matches the user PID {participant_id}"))
-                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"), "context": "victim"}
+                    # print(str(f"victimId is present and matches the user PID {participant_id}"))
+                    participant_events_dict[participant_events_this_minute] = {"event_type": event.get("type"),
+                                                                               "context": "victim"}
 
                 participant_events_this_minute = participant_events_this_minute + 1
 
             if len(participant_events_dict) > 0:
-                print(str(f"Event present!"))
+                # print(str(f"Event present!"))
                 events_per_minute[minute_count] = participant_events_dict
 
             # go to the next frame
@@ -98,11 +130,12 @@ try:
         lane_cs_previous_value = 0
         jungle_cs_previous_value = 0
         exp_previous_value = 0
+        gold_previous_value = 0
         frame_count = 0
         output_dict = {}
 
-        for frame in participant_events_per_minute:
-            participant_event = participant_events_per_minute[frame]
+        for frame in participant_frames_array:
+            participant_event = participant_frames_array[frame]
             lane_cs = participant_event[LANE_CS]
             jungle_cs = participant_event[JUNGLE_CS]
             total_cs = lane_cs + jungle_cs
@@ -111,6 +144,10 @@ try:
 
             exp_in_frame = participant_event["exp"]
             exp_frame_diff = exp_in_frame - exp_previous_value
+            level = participant_event["level"]
+
+            gold_in_frame = participant_event["gold"]
+            gold_frame_diff = gold_in_frame - gold_previous_value
 
             if frame in events_per_minute:
                 events = events_per_minute[frame]
@@ -118,15 +155,67 @@ try:
                 events = {"event_type": "None", "context": "None"}
 
             output_dict[frame_count] = {"total": total_cs, "lane_diff": lane_frame_diff,
-                                        "jungle_diff": jungle_frame_diff, "exp": exp_in_frame, "exp_diff": exp_frame_diff, "events": events}
+                                        "jungle_diff": jungle_frame_diff,
+                                        "exp": exp_in_frame, "level": level, "exp_diff": exp_frame_diff,
+                                        "gold": gold_in_frame, "gold_diff": gold_frame_diff,
+                                        "events": events}
 
             # increments
             lane_cs_previous_value = lane_cs_previous_value + lane_frame_diff
             jungle_cs_previous_value = jungle_cs_previous_value + jungle_frame_diff
             exp_previous_value = exp_previous_value + exp_frame_diff
+            gold_previous_value = gold_previous_value + gold_frame_diff
             frame_count = frame_count + 1
 
         return output_dict
+
+
+    def get_comparison(participant_id, match_id, champ_name):
+        # look up 1 or more skilled players on this champ from a static look up table
+        print(str(f"Getting experts for {champ_name}"))
+        results = champ_expert_mapper[champ_expert_mapper['champ_name'].str.lower() == champ_name.lower()]
+        # return 1 or more results
+        expert = results['expert'].values[0]
+        tag = results['tag'].values[0]
+        print(str(f"Getting puuid for {expert}:{tag}"))
+        expert_puuid = get_puuid_from_data(expert, tag)
+        print(str(f"Retrieved {expert_puuid}"))
+
+        expert_last_match = get_last_match(expert_puuid)
+        expert_match_detail = get_single_match_detail(expert_last_match, expert_puuid)
+        expert_participant_id = expert_match_detail.participantId
+        expert_cs_per_frame = get_cs_per_frame(expert_participant_id, expert_last_match)
+        user_cs_per_frame = get_cs_per_frame(participant_id, match_id)
+
+        # Find the size of the smaller list/array
+        size_expert_cs = len(expert_cs_per_frame)
+        size_user_cs = len(user_cs_per_frame)
+
+        comparison_frames_array = {}
+
+        # Determine the minimum size
+        min_size = min(size_expert_cs, size_user_cs)
+        for i in range(min_size):
+            # Access diffs from user cs array
+            user_lane_diff = user_cs_per_frame[i]['lane_diff']
+            user_jungle_diff = user_cs_per_frame[i]['jungle_diff']
+            user_total_diff = user_lane_diff + user_jungle_diff
+
+            # Access diffs from expert cs array
+            expert_lane_diff = expert_cs_per_frame[i]['lane_diff']
+            expert_jungle_diff = expert_cs_per_frame[i]['jungle_diff']
+            expert_total_diff = expert_lane_diff + expert_jungle_diff
+
+            comparison_frames_array[i] = {"your_cs_added": user_total_diff,
+                                          "your_exp_added": user_cs_per_frame[i]['exp_diff'],
+                                          "your_gold_added": user_cs_per_frame[i]['gold_diff'],
+                                          "expert_cs_added": expert_total_diff,
+                                          "expert_exp_added": expert_cs_per_frame[i]['exp_diff'],
+                                          "expert_gold_added": expert_cs_per_frame[i]['gold_diff']}
+
+        return comparison_frames_array
+
+
 
 except NotFoundError:
     print("cpmdetail: Not found!")
--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import CSRFProtect
 
+from cpmdetail import get_comparison
 from errors.not_found_error import NotFoundError
 from myforms.nameform import NameForm
 
@@ -17,7 +18,6 @@ app.secret_key = secret_key
 bootstrap = Bootstrap5(app)
 # Flask-WTF requires this line
 csrf = CSRFProtect(app)
-
 
 @app.route("/hello-world")
 def hello():
@@ -46,13 +46,23 @@ def index_post():
     return render_template('index.html', form=form, message="Invalid Form")
 
 
-@app.route("/<puuid>/<participant_id>/match/<match_id>")
-def match(puuid, participant_id, match_id):
-    message = str(f"CSM for {match_id}")
+@app.route("/<puuid>/<participant_id>/match/<match_id>/<champ_name>")
+def match(puuid, participant_id, match_id, champ_name):
+    message = str(f"CPM for a {champ_name} game")
     csm_detail = cpmdetail.get_cs_per_frame(participant_id, match_id)
     print(str(f"Getting {match_id} match for {puuid}:{participant_id}"))
-    return render_template('match-detail.html', message=message, csm_detail=csm_detail)
+    return render_template('match-detail.html', message=message, csm_detail=csm_detail, puuid=puuid, participant_id=participant_id, match_id=match_id, champ_name=champ_name)
 
+@app.route("/<puuid>/<participant_id>/match/<match_id>/<champ_name>/compare")
+def compare(puuid, participant_id, match_id, champ_name):
+
+    # average data or some other compare if more than one, then return data
+    compare_detail = get_comparison(participant_id, match_id, champ_name)
+
+    # fill in Subtitle via message
+    message = str(f"Comparing your game on {champ_name} to Experts")
+    # return page
+    return render_template('match-compare.html', message=message, compare_detail=compare_detail)
 
 if __name__ == "__main__":
     app.run(debug=True)
